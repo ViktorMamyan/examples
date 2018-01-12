36b2df709e8c1258927ea0f6db48b95a1b252f78
ViktorMamyan <viktormamyan@hotmail.com>
2018-01-12 15:28:48 +0400

    C# WCF examples added

---
+++ C#/WCF/Login/Windows/WCF Client Win Login/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+using System.Net;
+
+namespace WCF_Win_Auth_Login_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ignore ssl not trusted certificate 
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+
+            using (var client = new wcfService.iWCFClient())
+            { 
+                 //client.ClientCredentials.Windows.ClientCredential.UserName = "User";
+                //client.ClientCredentials.Windows.ClientCredential.Password = "Password";
+
+                client.Open();
+
+                client.iInfo();
+
+                //Console.WriteLine("IP: {0}",client.GetIP());
+
+                //Console.WriteLine( client.jInfo());
+
+                //Console.WriteLine(client.State.ToString());
+
+                Console.WriteLine(client.GetIP2());
+
+                client.Close();
+
+                Console.WriteLine("Finished");
+            }
+            
+            Console.ReadKey();
+
+        }
+
+    }
+}
---
+++ C#/WCF/Login/Windows/WCF Server Win Login/iWCF.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFApp
+{
+
+    //Interface
+
+    [ServiceContract]
+    public interface iWCF
+    {
+        [OperationContract]
+        void iInfo();
+
+        [OperationContract]
+        string jInfo();
+
+        [OperationContract]
+        string GetIP();
+
+        [OperationContract]
+        string GetIP2();
+
+    }
+
+    
+    // Implimentation Class
+
+    public class WCFClass : iWCF
+    {
+        public void iInfo()
+        {
+            Console.WriteLine("OK");
+        }
+
+        public string jInfo()
+        {
+            return "Data from server";
+        }
+
+        // should be checked
+        public string GetIP()
+        {
+            OperationContext context = OperationContext.Current;
+            System.ServiceModel.Channels.MessageProperties properties = context.IncomingMessageProperties;
+            System.ServiceModel.Channels.RemoteEndpointMessageProperty endpoint = properties[System.ServiceModel.Channels.RemoteEndpointMessageProperty.Name] as System.ServiceModel.Channels.RemoteEndpointMessageProperty;
+
+            string address = string.Empty;
+
+            if (properties.Keys.Contains(System.ServiceModel.Channels.HttpRequestMessageProperty.Name))
+            {
+                System.ServiceModel.Channels.HttpRequestMessageProperty endpointLoadBalancer = properties[System.ServiceModel.Channels.HttpRequestMessageProperty.Name] as System.ServiceModel.Channels.HttpRequestMessageProperty;
+                if (endpointLoadBalancer != null && endpointLoadBalancer.Headers["X-Forwarded-For"] != null)
+                    address = endpointLoadBalancer.Headers["X-Forwarded-For"];
+            }
+
+            if (string.IsNullOrEmpty(address))
+            {
+                address = endpoint.Address;
+            }
+
+            return address;
+        }
+
+        // should be checked
+        public string GetIP2()
+        {
+            OperationContext oOperationContext = OperationContext.Current;
+            System.ServiceModel.Channels.MessageProperties oMessageProperties = oOperationContext.IncomingMessageProperties;
+            System.ServiceModel.Channels.RemoteEndpointMessageProperty oRemoteEndpointMessageProperty = (System.ServiceModel.Channels.RemoteEndpointMessageProperty)oMessageProperties[System.ServiceModel.Channels.RemoteEndpointMessageProperty.Name];
+
+            string szAddress = oRemoteEndpointMessageProperty.Address;
+            int nPort = oRemoteEndpointMessageProperty.Port;
+
+            return "IP " + szAddress + " Port " + nPort.ToString();
+        }
+
+    }
+
+}
