9f2bde1f9b08c8e4bab5688b767011f082dffe55
ViktorMamyan <viktormamyan@hotmail.com>
2018-01-15 17:23:27 +0400

    DataContract example added

---
+++ C#/WCF/DataContract/Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using Client.hRef;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ignore ssl not trusted certificate 
+            //ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+
+            var http_client = new hRef.CarListClient("bhttp");
+            http_client.Open();
+
+            Car c = new Car();
+            c.ՀՀ = 5;
+            c.Model = "Model";
+            c.Vendor = "Vendor";
+            c.Year = 2018;
+
+            http_client.SetCar(c);
+
+            Console.WriteLine("Set");
+
+            c = http_client.GetCar(1);
+
+            Console.WriteLine("Set");
+
+            Console.WriteLine(c.ToString());
+
+            http_client.Close();
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/WCF/DataContract/Server/IWcfService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+using System.Xml.Linq;
+using System.Configuration;
+
+[DataContract]
+public class Car
+{
+    [DataMember(Order=1,Name="ՀՀ")]
+    public int ID { get; set; }
+
+    [DataMember(Order = 2)]
+    public string Vendor { get; set; }
+
+    [DataMember(Order = 3)]
+    public string Model { get; set; }
+
+    [DataMember(Order = 4)]
+    public int Year { get; set; }
+}
+
+namespace Server
+{
+
+    [ServiceContract(Name="CarList")]
+    public interface iCar
+    {
+
+        [OperationContract]
+        void SetCar(Car car);
+
+        [OperationContract]
+        Car GetCar(int id);
+    
+    }
+
+}
+
+namespace Server
+{
+
+    public class WcfService : iCar
+    {
+        public void SetCar(Car car)
+        {
+            var file = "cars.xml"; //ConfigurationManager.AppSettings["filecar"];
+
+            var doc = XDocument.Load(file);
+            doc.Root.Add(new XElement("Car", new XAttribute("ID", car.ID)
+                                            , new XAttribute("Vendor", car.Vendor)
+                                            , new XAttribute("Model", car.Model)
+                                            , new XAttribute("Year", car.Year)));
+
+            doc.Save(file);
+
+        }
+
+        public Car GetCar(int id)
+        {
+            var file = "cars.xml"; //ConfigurationManager.AppSettings["filecar"];
+            var result = new Car();
+            var doc = XDocument.Load(file);
+
+            var element = doc.Descendants("Car").FirstOrDefault(x => x.Attribute("ID").Value == id.ToString());
+
+            result.ID = int.Parse(element.Attribute("ID").Value);
+            result.Vendor = element.Attribute("Vendor").Value;
+            result.Model = element.Attribute("Model").Value;
+            result.Year = int.Parse(element.Attribute("Year").Value);
+
+            return result;
+        }
+    }
+
+}
