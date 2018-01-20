07464b7e4a49112d2130a3f206203eb02eeee3d7
ViktorMamyan <viktormamyan@hotmail.com>
2018-01-20 13:49:22 +0400

    ADO.NET examples added

---
+++ C#/ADO.NET/SQL Connection Pooling/SQL Connection/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnectionStringBuilder ConnectionBuilder = new SqlConnectionStringBuilder
+            {
+                DataSource = @"(local)\V",
+                InitialCatalog = "AdventureWorks2014",
+                IntegratedSecurity=true,
+                Pooling = true, // if true connection will not be closed
+                UserID="",
+                Password=""
+            };
+
+            using (SqlConnection connection = new SqlConnection(ConnectionBuilder.ConnectionString))
+            {
+                connection.StateChange+=connection_StateChange;
+                
+                try
+                {
+                    connection.Open();
+
+                    Console.WriteLine(connection.State);
+
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+
+        static void connection_StateChange(object sender, System.Data.StateChangeEventArgs e)
+        {
+            SqlConnection Conn = sender as SqlConnection;
+
+            Console.WriteLine("Database State: {0}\n", Conn.State);
+        }
+
+    }
+}
---
+++ C#/ADO.NET/SQL Connection/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ConnectionString = @"Data Source=(local)\V;Initial Catalog=AdventureWorks2014;Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.StateChange+=connection_StateChange;
+                
+                try
+                {
+                    connection.Open();
+
+                    Console.WriteLine(connection.State);
+
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+
+        static void connection_StateChange(object sender, System.Data.StateChangeEventArgs e)
+        {
+            SqlConnection Conn = sender as SqlConnection;
+
+            Console.WriteLine("Database State: {0}\n", Conn.State);
+        }
+
+    }
+}
