25c5413737286be6d73d5f0f055a0c5f4dc5814a
ViktorMamyan <viktormamyan@hotmail.com>
2018-01-21 11:09:43 +0400

    SQL Command added

---
+++ C#/ADO.NET/SQL Command Examples/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //using QueryToSqlServer with no parameters
+            DataTable dt = dbclass.QueryToSqlServer("Select 1 AS c;", CommandType.Text);
+            Console.WriteLine(dt.Rows[0][0].ToString());
+
+            //using QueryToSqlServer with parameters
+            List<SqlParameter> Parameters = new List<SqlParameter>();
+            Parameters.Add(new SqlParameter("@a", "a"));
+            Parameters.Add(new SqlParameter("@b", "b"));
+
+            DataTable dt2 = dbclass.QueryToSqlServer("Select @a,@b AS c;", CommandType.Text,Parameters.ToArray());
+            Console.WriteLine(dt2.Rows[0][0].ToString());
+
+            //using ExecuteSQL with no parameters
+            dbclass.ExecuteSQL("UPDATE table SET [Status] = 1;", CommandType.Text);
+
+            //using ExecuteSQL with parameters
+            List<SqlParameter> Parameters2 = new List<SqlParameter>();
+            Parameters2.Add(new SqlParameter("@a", "a"));
+            Parameters2.Add(new SqlParameter("@b", "b"));
+            Parameters2.Add(new SqlParameter("@c", "c"));
+            Parameters2.Add(new SqlParameter("@d", "d"));
+            dbclass.ExecuteSQL("ProcedureName", CommandType.StoredProcedure, Parameters2.ToArray());
+
+            //using QueryScalar with no parameters
+            object o = dbclass.QueryScalar("SELECT dbo.FunctionName();");
+
+            //using QueryScalar with parameters
+            List<SqlParameter> Parameters3 = new List<SqlParameter>();
+            Parameters3.Add(new SqlParameter("@a", "a"));
+            Parameters3.Add(new SqlParameter("@b", "b"));
+            object o2 = dbclass.QueryScalar("SELECT dbo.FunctionName(@a,@b);");
+
+            //passing datatable variable as parameter
+            DataTable T=new DataTable();//set datatable values
+            List<SqlParameter> Parameters4 = new List<SqlParameter>();
+            Parameters4.Add(new SqlParameter("@a", "a"));
+            SqlParameter p = new SqlParameter("@DataTableVariable", T)
+            {
+                TypeName="dbo.TypeName"
+            };
+            dbclass.ExecuteSQL("ProcedureName", CommandType.StoredProcedure, Parameters4.ToArray());
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ C#/ADO.NET/SQL Command Examples/dbclass.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL
+{
+    public static class dbclass
+    {
+        
+        static SqlConnectionStringBuilder ConnectionBuilder = new SqlConnectionStringBuilder
+        {
+            DataSource = @"(local)\V",
+            InitialCatalog = "AdventureWorks2014",
+            IntegratedSecurity = true,
+            Pooling = true, // if true connection will not be closed
+            UserID = "",
+            Password = ""
+        };
+
+        //return query with no parameters
+        public static DataTable QueryToSqlServer(string CommandText, CommandType SqlCommandType)
+        {
+            DataTable dt = new DataTable();
+
+            using (SqlConnection cnn = new SqlConnection(ConnectionBuilder.ConnectionString))
+            {
+                cnn.Open();
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = cnn;
+                    cmd.CommandType = SqlCommandType;
+                    cmd.CommandText = CommandText;
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        da.Fill(dt);
+                    }
+                }
+                cnn.Close();
+            }
+            return dt;
+        }
+
+        //return query with parameters
+        public static DataTable QueryToSqlServer(string CommandText, CommandType SqlCommandType, SqlParameter[] p)
+        {
+            DataTable dt = new DataTable();
+
+            using (SqlConnection cnn = new SqlConnection(ConnectionBuilder.ConnectionString))
+            {
+                cnn.Open();
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = cnn;
+                    cmd.CommandType = SqlCommandType;
+                    cmd.CommandText = CommandText;
+                    cmd.Parameters.AddRange(p);
+
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        da.Fill(dt);
+                    }
+                }
+                cnn.Close();
+            }
+            return dt;
+        }
+
+        //execute procedure with no parameters
+        public static void ExecuteSQL(string CommandText, CommandType SqlCommandType)
+        {
+            using (SqlConnection cnn = new SqlConnection(ConnectionBuilder.ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = cnn;
+                    cmd.CommandType = SqlCommandType;
+                    cmd.CommandText = CommandText;
+
+                    cnn.Open();
+                    cmd.ExecuteNonQuery();
+                    cnn.Close();
+                }
+            }
+        }
+
+        //exec procedure with parameters
+        public static void ExecuteSQL(string CommandText, CommandType SqlCommandType, SqlParameter[] p)
+        {
+            using (SqlConnection cnn = new SqlConnection(ConnectionBuilder.ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = cnn;
+                    cmd.CommandType = SqlCommandType;
+                    cmd.CommandText = CommandText;
+
+                    cmd.Parameters.AddRange(p);
+
+                    cnn.Open();
+                    cmd.ExecuteNonQuery();
+                    cnn.Close();
+                }
+            }
+        }
+
+        //return scalar query with no parameters
+        public static object QueryScalar(string CommandText)
+        {
+            using (SqlConnection cnn = new SqlConnection(ConnectionBuilder.ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = cnn;
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = CommandText;
+
+                    cnn.Open();
+                    object o = cmd.ExecuteScalar();
+                    cnn.Close();
+
+                    return o;
+                }
+            }
+        }
+
+        //return scalar query with parameters
+        public static object QueryScalar(string CommandText, SqlParameter[] p)
+        {
+            using (SqlConnection cnn = new SqlConnection(ConnectionBuilder.ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = cnn;
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = CommandText;
+                    cmd.Parameters.AddRange(p);
+
+                    cnn.Open();
+                    object o = cmd.ExecuteScalar();
+                    cnn.Close();
+
+                    return o;
+                }
+            }
+        }
+
+    }
+}
