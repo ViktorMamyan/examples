e2c71f4c5e2e5522d90d600bf13bbdebbb3e13b4
ViktorMamyan <viktormamyan@hotmail.com>
2018-02-06 11:00:27 +0400

    Linq samples added

---
+++ C#/LINQ/Grouping/Join and Group/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = GetCars();
+            var repairs = GetRepairs();
+            var carsWithRepairs = from c in cars
+                                  join rep in repairs
+                                  on c.VIN equals rep.VIN into temp
+                                  from r in temp.DefaultIfEmpty()
+                                  group r by new { c.VIN, c.Make } into grouped
+                                  select new
+                                  {
+                                      VIN = grouped.Key.VIN,
+                                      Make = grouped.Key.Make,
+                                      TotalCost = grouped.Sum(c => c == null ? 0 : c.Cost)
+                                  };
+            foreach (var item in carsWithRepairs)
+            {
+                Console.WriteLine("Car VIN:{0}, Make:{1}, Total Cost:{2:C}",
+                item.VIN, item.Make, item.TotalCost);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static List<Car> GetCars()
+            {
+                return new List<Car>
+                    {
+                        new Car {VIN = "ABC123",Make = "Ford",
+                            Model = "F-250", Year = 2000},
+                        new Car {VIN = "DEF123",Make = "BMW",
+                        Model = "Z-3", Year = 2005},
+                        new Car {VIN = "ABC456",Make = "Audi",
+                        Model = "TT", Year = 2008},
+                        new Car {VIN = "HIJ123",Make = "VW",
+                        Model = "Bug", Year = 1956},
+                        new Car {VIN = "DEF456",Make = "Ford",
+                        Model = "F-150", Year = 1998}
+                    };
+            }
+
+        private static List<Repair> GetRepairs()
+            {
+                return new List<Repair>
+                    {
+                        new Repair {VIN = "ABC123", Desc = "Change Oil", Cost = 29.99m},new Repair {VIN = "DEF123", Desc = "Rotate Tires", Cost =19.99m},
+                        new Repair {VIN = "HIJ123", Desc = "Replace Brakes", Cost = 200},
+                        new Repair {VIN = "DEF456", Desc = "Alignment", Cost = 30},
+                        new Repair {VIN = "ABC123", Desc = "Fix Flat Tire", Cost = 15},
+                        new Repair {VIN = "DEF123", Desc = "Fix Windshield", Cost =420},
+                        new Repair {VIN = "ABC123", Desc = "Replace Wipers", Cost = 20},
+                        new Repair {VIN = "HIJ123", Desc = "Replace Tires", Cost = 1000},
+                        new Repair {VIN = "DEF456", Desc = "Change Oil", Cost = 30}
+                    };
+            }
+
+    }
+
+    public class Car
+    {
+        public string VIN { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+    }
+
+    public class Repair
+    {
+        public string VIN { get; set; }
+        public string Desc { get; set; }
+        public decimal Cost { get; set; }
+    }
+
+}
---
+++ C#/LINQ/Where in loop part/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var schedule = GetDates();
+            var areYouAvailable = new DateTime(11, 7, 5);
+
+            //Linq query
+            var busy = from d in schedule
+                       where d == areYouAvailable
+                       select d;
+            foreach (var busyDate in busy)
+            {
+                Console.WriteLine("Sorry, but I am busy on {0:dd/MM/yyyy}", busyDate);
+            }
+
+
+            //where in loop part
+            foreach (var busyDate in schedule.Where(d => d == areYouAvailable))
+            {
+                Console.WriteLine("Sorry, but I am busy on {0:dd/MM/yyyy}", busyDate);
+            }
+
+            var count = (from d in schedule
+                       where d == areYouAvailable
+                       select d).Count();
+
+            Console.WriteLine("Busy days: " + count);
+
+            Console.ReadKey();
+        }
+
+        private static List<DateTime> GetDates()
+        {
+            return new List<DateTime>
+                {
+                new DateTime(11, 1, 1),
+                new DateTime(11, 2, 5),
+                new DateTime(11, 3, 3),
+                new DateTime(11, 1, 3),
+                new DateTime(11, 1, 2),
+                new DateTime(11, 5, 4),
+                new DateTime(11, 2, 2),
+                new DateTime(11, 7, 5),
+                new DateTime(11, 6, 30),
+                new DateTime(11, 10, 14),
+                new DateTime(11, 11, 22),
+                new DateTime(11, 12, 1),
+                new DateTime(11, 5, 22),
+                new DateTime(11, 6, 7),
+                new DateTime(11, 1, 4)
+                };
+        }
+
+    }
+}
