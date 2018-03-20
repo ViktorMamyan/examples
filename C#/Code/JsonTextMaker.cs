bc7f2e71b157642a75ccd7f603bfa27f6d757e45
ViktorMamyan <viktormamyan@hotmail.com>
2018-03-20 09:32:45 +0400

    C# example added

---
+++ C#/Code/JsonTextMaker.cs
@@ -0,0 +1,45 @@
+﻿using ExtensionMethods;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+//Install-Package Newtonsoft.Json
+
+namespace JsonMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create List of Class
+            List<Person> people = new List<Person>{
+                   new Person{ID = 1, FirstName = "Scott", LastName = "Gurthie"},
+                   new Person{ID = 2, FirstName = "Bill", LastName = "Gates"}
+                   };
+
+            //Convert to JSON string
+            string jsonString = people.ToJSON();
+
+            System.Console.WriteLine(jsonString);
+        }
+
+        //Custom Class
+        private class Person
+        {
+            public int ID { get; internal set; }
+            public string FirstName { get; internal set; }
+            public string LastName { get; internal set; }
+        }
+    }
+}
+
+//Extension Method
+namespace ExtensionMethods
+{
+    public static class JSONHelper
+    {
+        public static string ToJSON(this object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+    }
+}
