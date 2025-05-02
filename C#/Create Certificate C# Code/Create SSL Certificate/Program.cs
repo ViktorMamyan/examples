4cd3b7d1312ce87b5746ab94a756d4e6cd153fac
Viktor <viktormamyan@hotmail.com>
2025-05-02 11:03:26 +0400

    Code Changed

--- C#/Create Certificate C# Code/Create SSL Certificate/Program.cs
+++ C#/Create Certificate C# Code/Create SSL Certificate/Program.cs
@@ -15,14 +15,24 @@ namespace Create_SSL_Certificate
 
             CreateCert();
 
-            if (CheckIfRuningAsAdministrator() == false)
-            {
-                Console.WriteLine("Admin Required");
-                
-                return;
-            }
+            GetThumbprint();
 
-            InstalCert();
+            //CheckIfCertificateInstalled2("abc.com");
+
+            //string thumbprint = "3F900CFE734BD3320A962E7D83C6D5C31C47B1F9";
+            //bool IsInstalled = CheckIfCertificateInstalled(thumbprint);
+            //Console.WriteLine(IsInstalled);
+
+            
+
+            //if (CheckIfRuningAsAdministrator() == false)
+            //{
+            //    Console.WriteLine("Admin Required");
+
+            //    return;
+            //}
+
+            //InstalCert();
 
             Console.WriteLine("Finished");
             Console.ReadKey();
@@ -96,5 +106,65 @@ namespace Create_SSL_Certificate
 
             return isAdmin;
         }
+
+        static bool CheckIfCertificateInstalled(string thumbprint)
+        {
+            thumbprint = thumbprint.Replace(" ", "").ToUpperInvariant();
+
+            using (var store = new X509Store(StoreName.My, StoreLocation.LocalMachine))
+            {
+                store.Open(OpenFlags.ReadOnly);
+                var certs = store.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, validOnly: false);
+
+                if (certs.Count > 0)
+                {
+                    Console.WriteLine("Certificate is already installed.");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Certificate not found.");
+                    return false;
+                }
+            }
+        }
+
+        static bool CheckIfCertificateInstalled2(string domain)
+        {
+            using (var store = new X509Store(StoreName.My, StoreLocation.LocalMachine))
+            {
+                store.Open(OpenFlags.ReadOnly);
+                //store.Certificates.Find(X509FindType.FindBySubjectName, "mydomain.com", false);
+
+                var certs = store.Certificates.Find(X509FindType.FindBySubjectName, domain, false);
+
+                if (certs.Count > 0)
+                {
+                    Console.WriteLine("Certificate is already installed.");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Certificate not found.");
+                    return false;
+                }
+            }
+        }
+
+        //Get Thumbprint from .pfx or .cer
+        static string GetThumbprint()
+        {
+            string certPath = "mycert.pfx";           // or "mycert.cer"
+            string certPassword = "password123";      // needed only for .pfx files
+
+            var cert = new X509Certificate2(certPath, certPassword);
+
+            Console.WriteLine("Thumbprint: " + cert.Thumbprint);
+
+            string thumbprint = cert.Thumbprint?.Replace(" ", "").ToUpperInvariant();
+
+            return thumbprint;
+        }
+
     }
 }
